f838bc237ea2435a44b8e3423d55ac359c85350a
VanPTC <vanptc@rikkeisoft.com>
2025-02-12 15:28:44 +0700

    FE

--- FE/src/components/HelloWorld.vue
+++ FE/src/components/HelloWorld.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <h1>Upload CSV File System Demo</h1>
-    <input type="file" @change="handleFileUpload" accept=".csv" />
-    <button @click="uploadFile">Upload</button>
+    <h1>Demo Upload CSV File</h1>
+    <input
+      class="input-button"
+      type="file"
+      @change="handleFileUpload"
+      accept=".csv"
+    />
+    <button class="button" @click="uploadFile">Upload</button>
     <p v-if="message">{{ message }}</p>
   </div>
 </template>
@@ -63,5 +68,56 @@ li {
 }
 a {
   color: #42b983;
+}
+.button {
+  display: inline-block;
+  padding: 15px 24px;
+  margin-left: 10px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  text-transform: uppercase;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  background: linear-gradient(135deg, #ff4d4d, #b30000);
+  color: white;
+  box-shadow: 0 4px 10px rgba(255, 77, 77, 0.2);
+}
+
+.button:hover {
+  background: linear-gradient(135deg, #b30000, #800000);
+  box-shadow: 0 6px 15px rgba(255, 77, 77, 0.3);
+}
+
+.button:active {
+  transform: scale(0.95);
+  box-shadow: 0 3px 8px rgba(255, 77, 77, 0.2);
+}
+.input-button {
+  display: inline-block;
+  padding: 12px 24px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  text-transform: uppercase;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  background: linear-gradient(135deg, #ff4d4d, #b30000);
+  color: white;
+  box-shadow: 0 4px 10px rgba(255, 77, 77, 0.2);
+}
+
+.input-button:hover {
+  background: linear-gradient(135deg, #b30000, #800000);
+  box-shadow: 0 6px 15px rgba(255, 77, 77, 0.3);
+}
+
+.input-button:active {
+  transform: scale(0.95);
+  box-shadow: 0 3px 8px rgba(255, 77, 77, 0.2);
 }
 </style>
